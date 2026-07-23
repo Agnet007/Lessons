f878bb38ba093651275768ae3c8ec867a52d4712
Agnet007 <108275314+Agnet007@users.noreply.github.com>
2026-07-23 15:32:10 +0200

    Delete math-lessons directory

--- math-lessons/js/bootstrap.js
+++
@@ -1,1 +0,0 @@
-import{LessonApp}from'./app.js';import{TaskEngine}from'./tasks.js';import{DemoEngine}from'./demos.js';const id=new URLSearchParams(location.search).get('lesson')||'01-linear';const lesson=await fetch(`./lessons/${id}.json`).then(r=>r.json());new LessonApp(document.getElementById('lessonRoot'),lesson).mount();new TaskEngine(lesson).mount();new DemoEngine().mount();
