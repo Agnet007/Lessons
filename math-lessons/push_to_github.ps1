0fa0d71d1579785fc1996518a8bb80497742006d
Agnet007 <108275314+Agnet007@users.noreply.github.com>
2026-07-23 14:46:05 +0200

    Delete math-lessons directory

--- math-lessons/push_to_github.ps1
+++
@@ -1,7 +0,0 @@
-param([Parameter(Mandatory=$true)][string]$RepositoryUrl)
-git init
-git branch -M main
-git add .
-git commit -m "Initial GitHub Pages deployment"
-git remote add origin $RepositoryUrl
-git push -u origin main
